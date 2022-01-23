dfbece44b578e7d72a58e6d84d7e8b518fa04ad5
ntsk <ridha.h2a26@gmail.com>
2022-01-23 19:26:26 +0900

    Check if the metadata file is empty

--- lib/fastlane/plugin/amazon_appstore/helper/amazon_appstore_helper.rb
+++ lib/fastlane/plugin/amazon_appstore/helper/amazon_appstore_helper.rb
@@ -167,12 +167,12 @@ module Fastlane
         return changelog_text if skip_upload_changelogs
 
         path = File.join(metadata_path, language, 'changelogs', "#{version_code}.txt")
-        if File.exist?(path)
+        if File.exist?(path) && !File.zero?(path)
           UI.message("Updating changelog for '#{version_code}' and language '#{language}'...")
           changelog_text = File.read(path, encoding: 'UTF-8')
         else
           defalut_changelog_path = File.join(metadata_path, language, 'changelogs', 'default.txt')
-          if File.exist?(defalut_changelog_path)
+          if File.exist?(defalut_changelog_path) && !File.zero?(defalut_changelog_path)
             UI.message("Updating changelog for '#{version_code}' and language '#{language}' to default changelog...")
             changelog_text = File.read(defalut_changelog_path, encoding: 'UTF-8')
           else
